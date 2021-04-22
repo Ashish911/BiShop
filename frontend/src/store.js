d8b7e405c2907adc6c4c3a8b9767a9be0425f3cd
Ashish <ashishDongol32@gmail.com>
2021-04-22 21:32:58 +0545

    product detail done

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducer } from './reducers/prductReducers';
+import {
+  productDetailReducer,
+  productListReducer
+} from './reducers/prductReducers';
 
 const reducer = combineReducers({
-  productList: productListReducer
+  productList: productListReducer,
+  productDetails: productDetailReducer
 });
 
 const initialState = {};
