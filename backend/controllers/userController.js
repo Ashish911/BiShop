3f37490f84d93e485a9e692f3c34edd2a3abf205
Ashish <ashishDongol32@gmail.com>
2021-04-24 18:32:28 +0545

    auth middleware implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,4 +23,21 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// Get User Profile '/api/users/profile' GET Request
+// Private Route
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    });
+  } else {
+    res.status(404);
+    throw new Error('User Not Found');
+  }
+});
+
+export { authUser, getUserProfile };
