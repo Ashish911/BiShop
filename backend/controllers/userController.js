04c6b9b6116561ed94a25d4cc5e2982ac22e7ee5
Ashish <ashishDongol32@gmail.com>
2021-05-06 20:49:46 +0545

    user delete done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -105,4 +105,29 @@ const getUsers = asyncHandler(async (req, res) => {
   res.json(users);
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
+// Delete user by id '/api/users/:id' DELETE Request
+// Private/Admin Route
+const deleteUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    await user.remove();
+    res.json({
+      message: 'User Removed'
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+
+  res.json(users);
+});
+
+export {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUserById
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,6 +7,7 @@ import {
 } from './reducers/prductReducers';
 import { cartReducer } from './reducers/cartReducers';
 import {
+  userDeleteReducer,
   userDetailsReducer,
   userListReducer,
   userLoginReducer,
@@ -29,6 +30,7 @@ const reducer = combineReducers({
   userDetail: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   userList: userListReducer,
+  userDelete: userDeleteReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
