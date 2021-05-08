ce4232d2c583a578832493a46f9fdced83a3be8c
Ashish <ashishDongol32@gmail.com>
2021-05-08 18:42:07 +0545

    upload endpoint created

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
@@ -8,6 +9,7 @@ import connectDB from './config/db.js';
 import productRoute from './routes/productRoute.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 
@@ -26,10 +28,15 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoute);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+// For require syntax
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 
