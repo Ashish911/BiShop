f2c104c237cc8a9702184402f164b7f494937a1a
Ashish <ashishDongol32@gmail.com>
2021-05-09 21:03:19 +0545

    pagination completed

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,6 +4,9 @@ import Product from '../models/product.js';
 // Get all Products '/api/products/' GET Request
 // Public Route
 const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = 2;
+  const page = Number(req.query.pageNumber) || 1;
+
   const keyword = req.query.keyword
     ? {
         name: {
@@ -13,8 +16,17 @@ const getProducts = asyncHandler(async (req, res) => {
       }
     : {};
 
-  const products = await Product.find({ ...keyword });
-  res.json(products);
+  const count = await Product.countDocuments({ ...keyword });
+
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+
+  res.json({
+    products,
+    page,
+    pages: Math.ceil(count / pageSize)
+  });
 });
 
 // Get Product by Id '/api/products/:id' GET Request
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -22,13 +22,17 @@ import {
 import { logout } from './userActions';
 
 // This is redux thunk using async function within a function
-export const listProducts = (keyword = '') => async (dispatch) => {
+export const listProducts = (keyword = '', pageNumber = '') => async (
+  dispatch
+) => {
   try {
     dispatch({
       type: PRODUCT_LIST_REQUEST
     });
 
-    const { data } = await axios.get(`/api/products?keyword=${keyword}`);
+    const { data } = await axios.get(
+      `/api/products?keyword=${keyword}&pageNumber=${pageNumber}`
+    );
 
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
